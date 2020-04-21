06b0566c05613b7cc719055be52b892f3e95931c
redhyper <51200428+redhyper@users.noreply.github.com>
2020-04-21 21:19:45 +0600

    Add files via upload

---
+++ food/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from food.models import *
+
+# admin.site.register(Table)
+# admin.site.register(Role)
+# admin.site.register(Department)
+# admin.site.register(User)
+# admin.site.register(MealsCategory)
+# admin.site.register(Status)
+# admin.site.register(ServicePercentage)
+# admin.site.register(Meal)
+# admin.site.register(Check)
+# admin.site.register(Order)
